f5fa2cf575c3613d8ee874c2343a8fa21ecc8989
jaeseonNamgung <sunnamgung8@naver.com>
2023-04-08 17:03:14 +0900

    update

--- src/main/java/sia/tacocloud/tacos/data/OrderRepository.java
+++ src/main/java/sia/tacocloud/tacos/data/OrderRepository.java
@@ -1,15 +1,13 @@
 package sia.tacocloud.tacos.data;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
-import sia.tacocloud.tacos.Ingredient;
 import sia.tacocloud.tacos.Order;
 import sia.tacocloud.tacos.User;
 
 import java.util.List;
 
-public interface OrderRepository extends CrudRepository<Order, String> {
+public interface OrderRepository extends CrudRepository<Order, Long> {
 
     List<Order> findByUserOrderByPlacedAtDesc(User user, Pageable pageable);
 }
--- src/main/java/sia/tacocloud/tacos/data/TacoRepository.java
+++ src/main/java/sia/tacocloud/tacos/data/TacoRepository.java
@@ -1,8 +1,8 @@
 package sia.tacocloud.tacos.data;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import sia.tacocloud.tacos.Taco;
 
-public interface TacoRepository extends CrudRepository<Taco, String> {
+public interface TacoRepository extends JpaRepository<Taco, Long> {
 
 }
